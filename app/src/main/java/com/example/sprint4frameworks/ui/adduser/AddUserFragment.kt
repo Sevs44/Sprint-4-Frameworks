d445955b147e44e53521206337c975f5d0d45d60
Sevs44 <sebasbenenati@gmail.com>
2024-07-13 02:15:59 +0200

    Can now add users to the database successfully.

--- app/src/main/java/com/example/sprint4frameworks/ui/adduser/AddUserFragment.kt
+++ app/src/main/java/com/example/sprint4frameworks/ui/adduser/AddUserFragment.kt
@@ -7,11 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.sprint4frameworks.data.models.City
 import com.example.sprint4frameworks.databinding.FragmentAddUserBinding
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,26 +28,74 @@ class AddUserFragment : Fragment() {
     ): View {
         val cities = viewModel.getCities()
 
+        getLocation()
+
         binding = FragmentAddUserBinding.inflate(inflater, container, false)
 
         binding.ibBack.setOnClickListener {
             findNavController().navigateUp()
         }
         binding.btSaveUser.setOnClickListener {
-            addUser()
+            addUser(cities)
         }
         setUpSpinner(cities)
 
         return binding.root
     }
 
-    private fun addUser() {
-        //TODO
+    private fun addUser(cities: List<City>) {
+        val name = binding.etName.text.toString()
+        val favColor = binding.etFavColor.text.toString()
+        val birthdate = binding.etBirthdate.text.toString()
+        val favCityName = binding.spinnerFavCity.selectedItem.toString()
+        val favCityLat =
+            getCity(cities, binding.spinnerFavCity.onItemSelectedListener.toString()).lat
+        val favCityLng =
+            getCity(cities, binding.spinnerFavCity.onItemSelectedListener.toString()).lng
+        val favNumber = binding.etFavNumber.text.toString()
+        val actualLocationLat = getLocation().latitude
+        val actualLocationLng = getLocation().longitude
+        if (viewModel.checkFields(
+                name,
+                favColor,
+                birthdate,
+                favCityLat,
+                favCityLng,
+                favCityName,
+                favNumber,
+                actualLocationLat,
+                actualLocationLng
+            )
+        ) {
+            viewModel.addUser(
+                name,
+                favColor,
+                birthdate,
+                favCityLat,
+                favCityLng,
+                favCityName,
+                favNumber.toInt(),
+                actualLocationLat,
+                actualLocationLng
+            )
+
+            findNavController().navigateUp()
+        } else
+            Toast.makeText(context, "Fill all fields", Toast.LENGTH_LONG).show()
     }
 
-    private fun getLocation() {
-        //TODO
+    private fun getCity(cities: List<City>, cityName: String): City {
+        val city: City
+        val unknownCity = cities.find { it.name == cityName }
+        city = unknownCity?.copy() ?: cities[0]
+        return city
     }
+
+    private fun getLocation(): LatLng {
+        //TODO
+        return LatLng(40.7128, -74.0060)
+    }
+
 
     private fun setUpSpinner(cities: List<City>) {
         val adapter = ArrayAdapter(
@@ -64,11 +114,11 @@ class AddUserFragment : Fragment() {
                     position: Int,
                     id: Long
                 ) {
-                    var selectedCity = cities[position]
+                    cities[position]
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>) {
-                    var selectedCity = cities[0]
+                    cities[0]
                 }
             }
     }
--- app/src/main/java/com/example/sprint4frameworks/ui/adduser/AddUserViewModel.kt
+++ app/src/main/java/com/example/sprint4frameworks/ui/adduser/AddUserViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.sprint4frameworks.ui.adduser
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.sprint4frameworks.data.db.entities.UserEntity
 import com.example.sprint4frameworks.data.repositories.CitiesRepository
 import com.example.sprint4frameworks.data.repositories.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,6 +14,54 @@ class AddUserViewModel @Inject constructor(
     private val userRepository: UserRepository,
 ) :
     ViewModel() {
-    suspend fun addUser(user: UserEntity) = userRepository.insertUser(user)
     fun getCities() = CitiesRepository().getCities()
+    fun addUser(
+        name: String,
+        favColor: String,
+        birthdate: String,
+        favCityLat: Double,
+        favCityLng: Double,
+        favCityName: String,
+        favNumber: Int,
+        actualLocationLat: Double,
+        actualLocationLng: Double
+    ) {
+        viewModelScope.launch {
+            val user = UserEntity(
+                0,
+                name,
+                favColor,
+                birthdate,
+                favCityLat,
+                favCityLng,
+                favCityName,
+                favNumber,
+                actualLocationLat,
+                actualLocationLng
+            )
+            userRepository.insertUser(user)
+        }
+    }
+
+    fun checkFields(
+        name: String,
+        favColor: String,
+        birthdate: String,
+        favCityLat: Double,
+        favCityLng: Double,
+        favCityName: String,
+        favNumber: String,
+        actualLocationLat: Double,
+        actualLocationLng: Double
+    ): Boolean {
+        return (name == null ||
+                favColor == null ||
+                birthdate == null ||
+                favCityLat == null ||
+                favCityLng == null ||
+                favCityName == null ||
+                favNumber == null ||
+                actualLocationLat == null ||
+                actualLocationLng == null)
+    }
 }
